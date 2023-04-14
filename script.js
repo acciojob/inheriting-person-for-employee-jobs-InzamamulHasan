f06fb39eb706543810e1050f2ac2f8fa42f63d43
Inzamamul Hasan <107973468+InzamamulHasan@users.noreply.github.com>
2023-04-14 23:10:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ function Person(name, age) {
 	this.name=name;
 	this.age=age;
 }
-Person.prototype.jobGreet=function(){
-	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+Person.prototype.greet=function(){
+	console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`)
 }
 
 function Employee(name, age, jobTitle) {
@@ -12,7 +12,7 @@ function Employee(name, age, jobTitle) {
 	this.age=age;
 	this.jobTitle=jobTitle;
 }
-Employee.prototype.greet=function(){
+Employee.prototype.JobGreet=function(){
 	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
 }
 
